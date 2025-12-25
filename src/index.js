2ad000d4379bb01482df8b1f1d03f57eff28bd9b
pianellalilia-dot <pianella.lilia@gmail.com>
2025-12-25 10:44:34 +0100

    Added search functionality using API for city and temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function updateWeather(response) {
+  let currentCity = document.querySelector("#current-city");
+  let currentTemperature = document.querySelector(
+    "#current-temperature-number"
+  );
+  let temperature = Math.round(response.data.temperature.current);
+  currentCity.innerHTML = response.data.city;
+  currentTemperature.innerHTML = temperature;
+}
+
 function searchCity(city) {
+  let apiKey = "90303e49ba1c9f82cb5tf7o1afec334c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#search-form-input");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = inputElement.value;
+  searchCity(inputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
