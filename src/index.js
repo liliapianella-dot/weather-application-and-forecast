0ddddaec94ecbc133bf0e3e3e3ca984c0769ad04
pianellalilia-dot <pianella.lilia@gmail.com>
2025-12-25 11:34:38 +0100

    Added a city to start with

--- src/index.js
+++ src/index.js
@@ -32,5 +32,5 @@ function handleSearchSubmit(event) {
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Copenhagen");
